f9146818e60c86c8e542005c7cd3a124c060118c
GitUser1 <ghubname@gmail.com>
2020-11-05 16:34:32 +0100

    fetch string parameter from jenkins

--- src/main/java/fr/imiddleware/projets/App.java
+++ src/main/java/fr/imiddleware/projets/App.java
@@ -1,4 +1,5 @@
 package fr.imiddleware.projets;
+import java.util.logging.Logger;
 
 /**
  * Hello world!
@@ -8,6 +9,11 @@ public class App
 {
     public static void main( String[] args )
     {
+    	
         System.out.println( "Hello World!" );
+        String messageinput = System.getProperty("MessageInput");
+        
+        Logger log = Logger.getLogger("myFirstLog");
+        log.info("---------------" + messageinput);
     }
 }
