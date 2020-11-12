8ed125f4f800594ed187699e79e53a3e22f86067
GitUser1 <ghubname@gmail.com>
2020-11-12 14:52:04 +0100

    add system println

--- src/main/java/fr/imiddleware/projets/App.java
+++ src/main/java/fr/imiddleware/projets/App.java
@@ -1,19 +1,30 @@
 package fr.imiddleware.projets;
 import java.util.logging.Logger;
 
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
 /**
  * Hello world!
  *
  */
+
+@Mojo( name = "NewMavenGoal")
 public class App 
 {
     public static void main( String[] args )
     {
     	
-        System.out.println( "Hello World!" );
-        String messageinput = System.getProperty("MessageInput");
+        System.out.println( System.getProperty("message") );
+        String messageinput = System.getProperty("message");
+        
+        System.out.println("-------------------------------------");
+        System.out.println(messageinput);
         
         Logger log = Logger.getLogger("myFirstLog");
         log.info("---------------" + messageinput);
+        
+        
+
     }
 }
